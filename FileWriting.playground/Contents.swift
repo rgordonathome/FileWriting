5a89def17a7bf38a86d69705b9768ae676fcf9d0
Russell Gordon <rgordon@rsgc.on.ca>
2015-11-15 06:10:19 -0500

    Simple example of writing to file in shared playground documents folder.

---
+++ FileWriting.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: Playground - noun: a place where people can play
+
+// Obtained from:
+//
+// http://stackoverflow.com/questions/27327067/append-text-or-data-to-text-file-in-swift
+//
+// Minor modifications made by R. Gordon on 15-11-15.
+
+import Cocoa
+
+extension String {
+    func appendLineToURL(fileURL: NSURL) throws {
+        try self.stringByAppendingString("\n").appendToURL(fileURL)
+    }
+    
+    func appendToURL(fileURL: NSURL) throws {
+        let data = self.dataUsingEncoding(NSUTF8StringEncoding)!
+        try data.appendToURL(fileURL)
+    }
+}
+
+extension NSData {
+    func appendToURL(fileURL: NSURL) throws {
+        if let fileHandle = try? NSFileHandle(forWritingToURL: fileURL) {
+            defer {
+                fileHandle.closeFile()
+            }
+            fileHandle.seekToEndOfFile()
+            fileHandle.writeData(self)
+        }
+        else {
+            try writeToURL(fileURL, options: .DataWritingAtomic)
+        }
+    }
+}
+
+// Get path to the Documents folder
+let documentPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+
+// Append folder name of Shared Playground Data folder
+let sharedDataPath = documentPath.stringByAppendingPathComponent("Shared Playground Data")
+
+// Append file name to folder path string
+let filePath = sharedDataPath + "/output.txt"
+
+// Test
+do {
+    let url = NSURL(fileURLWithPath: filePath)
+    try "Test \(NSDate())".appendLineToURL(url)
+    let result = try String(contentsOfURL: url)
+}
+catch {
+    print("Could not write to file")
+}
